cb8cb3b28e0c9021fe63336ebc87f7a5236c5384
Shukhrat <shuhratick.abdurasulov@gmail.com>
2016-11-14 23:44:34 -1000

    commit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE "html">");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+// Загружаем модуль http
+var http = require('http');
+
+// Создаем web-сервер с обработчиком запросов
+var server = http.createServer(function (req, res) {
+    console.log('Начало обработки запроса');
+    // Передаем код ответа и http-заголовки
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=UTF-8'
+    });
+    res.end('Hello world!');
 });
 
-server.listen(80);
-console.log("Server is listening");
+// Запускаем web-сервер
+server.listen(3000);
